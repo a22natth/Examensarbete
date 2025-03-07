7759f44e085dd5e1dea4603fd58fb0fd39b61d0c
a22natth <a22natth@student.his.se>
2025-03-07 10:18:42 +0100

    Created lineChart.js
    This file contains the js code that creates the actual diagram. It also fetches data from the file data.json, but can't display the fetched data yet.

---
+++ canvasFolder/lineChart.js
@@ -0,0 +1,31 @@
+const ctx = document.getElementById('myChart');
+
+fetch('data.json')
+
+.then(function(data){
+    console.log(data)
+    createChart(data)
+})
+
+function createChart(data){
+
+  new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      },
+      maintainAspectRatio: false
+    }
+  });
+}
