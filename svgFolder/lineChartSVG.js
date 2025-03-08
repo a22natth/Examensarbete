acf4c8cde615a49e0ef813ecc1fba0813dbe150c
a22natth <a22natth@student.his.se>
2025-03-08 21:06:01 +0100

    Removed hardcoded data
    Removed hardcoded data and fetched data from the data.json file instead. Now the chart is able to display the correct test-data.

--- svgFolder/lineChartSVG.js
+++ svgFolder/lineChartSVG.js
@@ -1,23 +1,27 @@
-var data = {
-    // A labels array that can contain any sort of values
-    labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
-    // Our series array that contains series objects or in this case series data arrays
-    series: [
-      [5, 2, 4, 2, 0]
-    ]
+
+    fetch('../dataFolder/data.json')
+
+    .then(function(response){
+    if(response.ok == true){
+        return response.json();
+    }
+    })
+    // If response is ok then a chart is created with the fetched data from the JSON file
+    .then(function(climateData){
+    var data = {
+        labels: climateData.weatherdata.avgtemperatures.map(row => row.date),
+        series: [climateData.weatherdata.avgtemperatures.map(row => row.temp)],
+      };
+
+    // Customization of line chart
+    var options = {
+        width: 600,
+        height: 400,
+        // Doesn't draw the line chart points
+        showPoint: false,
+        // Disables line smoothing
+        lineSmooth: false,
   };
   
-// Customization of line chart
-  var options = {
-    width: 500,
-    height: 400,
-    // Don't draw the line chart points
-    showPoint: false,
-    // Disable line smoothing
-    lineSmooth: false,
-  };
-  
-  // Create a new line chart object where as first parameter we pass in a selector
-  // that is resolving to our chart container element. The Second parameter
-  // is the actual data object. As a third parameter we pass in our custom options.
   new Chartist.Line('.ct-chart', data, options);
+});
