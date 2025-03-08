7bd7d5180b89dfcd72d3d639cfb47307eefaa6d4
a22natth <a22natth@student.his.se>
2025-03-08 12:32:24 +0100

    Updated lineChart.js
    Updated lineChart.js with the ability to fetch just the dates from the JSON file and console.log them (in the process of using them in the actual diagram).

--- canvasFolder/lineChart.js
+++ canvasFolder/lineChart.js
@@ -1,12 +1,25 @@
 const ctx = document.getElementById('myChart');
 
-fetch('data.json')
+// Function that fetches the data from JSON file
+async function fetchData(){
+    const url = 'data.json'; // Idk if i need to change the url later...
+    const response = await fetch(url);
 
-.then(function(data){
-    console.log(data)
-    createChart(data)
-})
+    const datapoints = await response.json();
+    //console.log(datapoints);
+    return datapoints;
+};
 
+// Fetches just the dates (does not display them)
+fetchData().then(datapoints => {
+    const dates = datapoints.weatherdata.avgtemperatures.map(
+        function(index){
+            return index.date;
+        })
+    console.log(dates);
+});
+
+// Creates the linechart (yet to have the data from the JSON file though)
 function createChart(data){
 
   new Chart(ctx, {
