627af7f1c4fb80241653102c414564b30eb353d3
a22natth <a22natth@student.his.se>
2025-03-11 19:13:09 +0100

    Updated lineChartSVG.js with indentations and changed chart dimensions
    Updated lineChartSVG.js with indentations and changed chart dimensions

--- svgFolder/lineChartSVG.js
+++ svgFolder/lineChartSVG.js
@@ -1,13 +1,12 @@
+fetch('../dataFolder/dataSmall.json')
 
-    fetch('../dataFolder/data.json')
-
-    .then(function(response){
+.then(function(response){
     if(response.ok == true){
         return response.json();
     }
     })
     // If response is ok then a chart is created with the fetched data from the JSON file
-    .then(function(climateData){
+.then(function(climateData){
     var data = {
         labels: climateData.weatherdata.avgtemperatures.map(row => row.date),
         series: [climateData.weatherdata.avgtemperatures.map(row => row.temp)],
@@ -15,16 +14,19 @@
 
     // Customization of line chart
     var options = {
-        width: 600,
-        height: 400,
+        height: 650,
+        width: 1200,
+        // Makes sure this value is always on the axis
+        referenceValue: 5,
         // Doesn't draw the line chart points
         showPoint: false,
         // Disables line smoothing
         lineSmooth: false,
           axisY: {
-            offset: 60,
-          }
-  };
-  
+            offset: 30,
+            // Adjust scaleMinSpace to auto adjust values (decimals to integers only eg. on y-axis)
+            scaleMinSpace: 30,
+        },
+    };
   new Chartist.Line('.ct-chart', data, options);
 });
