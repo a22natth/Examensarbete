a0b3d38b6dc47df8bc5d0a288cebc19be11e6d8c
a22natth <a22natth@student.his.se>
2025-03-24 19:38:39 +0100

    Added MutationDetection to canvasFolder/lineChart.js
    Added MutationDetection functionality to have it listen to the chart-element during the render, and only when not detecting any mutations (modifications), it stops listening and the timer stops.

--- canvasFolder/barChart.js
+++ canvasFolder/barChart.js
@@ -1,7 +1,9 @@
 const ctx = document.getElementById('myChart');
+const parent = document.querySelector("#myChart");
+var before = 0;
 
 // Fetches data from specified file (although might have to change when using XAMPP)
-fetch('../dataFolder/dataSmall.json')
+fetch('../dataFolder/dataLarge.json')
 
 .then(function(response){
   if(response.ok == true){
@@ -10,13 +12,16 @@ fetch('../dataFolder/dataSmall.json')
 })
 // If response is ok then a chart is created with the fetched data from the JSON file
 .then(function(climateData){
-  createChart(climateData)
+  mutationDetection(parent, () => {
+    console.log("completed");
+  });
+  createChart(climateData);   
 });
 
 // Creates the linechart with passed data and takes current time 
 function createChart(climateData){
   // Takes starting time of rendering diagram
-  var before = performance.now();
+  before = performance.now();
   new Chart(ctx, {
     type: 'bar',
     data: {
@@ -49,17 +54,42 @@ function createChart(climateData){
     }
     
   });
-  // Takes new time when chart has finished drawing
-  // and calculates the total drawing time
-  var after = performance.now();
-  var ms = after - before;
-  localStorage.setItem("ms", ms);
-  console.log(ms);
+
 }
-  // Sets the value to stop so that the measuring script can
-  // reload page when the rendering is done
-  var stopped = true;
-  localStorage.setItem("stopValue",stopped);
+
+function mutationDetection(parent, callback){
+  let timer;
+  const now = new Date();
+  console.log("inside mutation" + now.getMilliseconds());
+  // Creates new MutationObserver object 
+  const observer = new MutationObserver(() => {
+    // Each time a modification(mutation) is detected within the element
+    // the timer is reset (i.e it will never reach 500ms)
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      // If timer is reached, i.e. no modifications have been
+      // made for the past 500 ms, the observer stops listening
+      // and takes the time for the stop-point of the chart-drawing
+      observer.disconnect();
+      console.log("stopping timer and this is before" + before);
+      var after = performance.now();
+      var ms = (after - before) - 500;
+      console.log(ms);
+      callback();  
+    }, 500);
+  });
+
+  // Lists what should be observed in regards to the chart-element
+  // And observes/listens any modifications within these
+  observer.observe(parent, {
+    childList: true,
+    attributes: true,
+    characterData: true,
+    subtree: true,
+    attributeOldValue: true,
+    characterDataOldValue: true
+  });
+}
 
 
 
